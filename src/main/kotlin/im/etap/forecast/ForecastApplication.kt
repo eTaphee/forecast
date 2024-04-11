0336c3111a132f79d990e1356bfb0438ac7622c9
thlee <e-tap@naver.com>
2024-04-11 19:47:59 +0900

    chore: 프로젝트 생성

---
+++ src/main/kotlin/im/etap/forecast/ForecastApplication.kt
@@ -0,0 +1,11 @@
+package im.etap.forecast
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForecastApplication
+
+fun main(args: Array<String>) {
+    runApplication<ForecastApplication>(*args)
+}
---
+++ src/test/kotlin/im/etap/forecast/ForecastApplicationTests.kt
@@ -0,0 +1,13 @@
+package im.etap.forecast
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForecastApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
