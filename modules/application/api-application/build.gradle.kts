78b2b8d510b06d8f25aa4a124be3575ca6015465
thlee <e-tap@naver.com>
2024-04-17 10:53:33 +0900

    feat: 단기예보 조회 api 추가

--- modules/application/api-application/build.gradle.kts
+++ modules/application/api-application/build.gradle.kts
@@ -9,5 +9,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.hibernate:hibernate-spatial:6.4.4.Final")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
---
+++ modules/application/api-application/src/main/kotlin/im/etap/forecast/application/api/controller/ForecastQueryController.kt
@@ -0,0 +1,45 @@
+package im.etap.forecast.application.api.controller
+
+import im.etap.forecast.application.api.dto.ForecastInfoResponse
+import im.etap.forecast.application.api.service.ForecastQueryService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * 기상예보 조회 컨트롤러
+ *
+ * @param forecastQueryService 기상예보 조회 서비스
+ */
+@RestController
+@RequestMapping("/forecast")
+class ForecastQueryController(
+    private val forecastQueryService: ForecastQueryService
+) {
+
+    /**
+     * 단기예보 조회
+     *
+     * @param lat 위도(33~38)
+     * @param lng 경도(124~132)
+     *
+     * @return 단기예보 정보 (없을 경우 NO_CONTENT)
+     */
+    @GetMapping("/village")
+    fun getVillageForecast(
+        lat: Double = 0.0,
+        lng: Double = 0.0
+    ): ResponseEntity<List<ForecastInfoResponse>> {
+        forecastQueryService.getVillageForecast(lat, lng)
+            .let {
+                val status = if (it.isEmpty()) {
+                    HttpStatus.NO_CONTENT
+                } else {
+                    HttpStatus.OK
+                }
+                return ResponseEntity.status(status).body(it)
+            }
+    }
+}
